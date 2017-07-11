3cb85b48acfc53371c32e0edd5fdacbdf5d5c6be
q14035 <e1q14035@st.oit.ac.jp>
2017-07-11 18:57:25 +0000

    Fix scripts/motors.py and test/travis_test_motors.py

--- test/travis_test_motors.py
+++ test/travis_test_motors.py
@@ -16,7 +16,7 @@ class MotorTest(unittest.TestCase):
 		on = rospy.ServiceProxy('/motor_on', Trigger)
 		ret = on()
 
-	def file_check(self, dev, value, message):aaaddsdff
+	def file_check(self, dev, value, message):
 		with open("/dev/" + dev, "r") as f:
 			self.assertEqual(f.readline(), str(value)+"\n", message)
 
